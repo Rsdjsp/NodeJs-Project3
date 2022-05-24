026420888259567d3720c05bb56ac69994ad1b4d
Robert Santos <rssp_96@hotmail.com>
2022-05-23 22:49:29 -0500

    CORS

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000","https://react-ecommerce-93fdd.web.app"],
   })
 );
 app.use(cookies());
